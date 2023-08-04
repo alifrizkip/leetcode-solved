4249d93bbb6441b38624866fc8762ee4ad907c70
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-04 14:33:55 +0700

    Time: 4 ms (93.48%), Space: 4.8 MB (97.83%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.go
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.go
@@ -1,24 +1,11 @@
 func sortArrayByParity(nums []int) []int {
-    var ans = make([]int,0)
-
-    for l := 0; l <= len(nums) / 2; l++ {
-        front := nums[l]
-        if front % 2 == 0 {
-            ans = append([]int{front}, ans...)
-        } else {
-            ans = append(ans, front)
-        }
-
-        // right not reach center
-        if len(nums)-1-l > len(nums) / 2 {
-            rear := nums[len(nums)-1-l]
-            if rear % 2 == 0 {
-                ans = append([]int{rear}, ans...)
-            } else {
-                ans = append(ans, rear)
-            }
+    var nextEvenIdx int
+    for i, n := range nums {
+        if n % 2 == 0 {
+            nums[i], nums[nextEvenIdx] = nums[nextEvenIdx], nums[i]
+            nextEvenIdx++
         }
     }
 
-    return ans
+    return nums
 }
