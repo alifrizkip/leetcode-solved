5fa71e7a91743fac07b3361d50e57b30a795e631
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-05 14:21:23 +0700

    Time: 847 ms (75.03%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,14 +1,7 @@
 # Write your MySQL query statement below
 select
     w.id
-from Weather w
+from Weather w, Weather y
 where
-    w.temperature > (
-        # yesterday
-        select
-            y.temperature
-        from Weather y
-        where y.recordDate = subdate(w.recordDate, 1)
-        order by y.recordDate desc
-        limit 1
-    )
+    datediff(w.recordDate, y.recordDate) = 1
+    and w.temperature > y.temperature
