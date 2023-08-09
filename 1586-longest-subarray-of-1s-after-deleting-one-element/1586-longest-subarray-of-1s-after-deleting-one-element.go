e799a97e58a83e88ab6d37f875bdb41297e0ee19
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-09 12:04:32 +0700

    Time: 49 ms (35.75%), Space: 7.5 MB (34.99%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.go
@@ -0,0 +1,29 @@
+
+func longestSubarray(nums []int) int {
+    var res int
+    var left, right int
+    var foundZero bool
+    for end := 0; end < len(nums); end++ {
+        n := nums[end]
+
+        if n == 1 {
+            right++
+
+            if left+right > res {
+                res = left+right
+            }
+        }
+
+        if n == 0 {
+            left = right
+            right = 0
+            foundZero = true
+        }
+    }
+
+    if !foundZero {
+        return len(nums)-1
+    }
+
+    return res
+}
