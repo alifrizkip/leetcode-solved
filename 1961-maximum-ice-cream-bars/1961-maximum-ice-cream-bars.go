37e74da626208e179a9fad8ea2020a910e69b9af
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-14 15:22:35 +0700

    Time: 114 ms (100%), Space: 9.2 MB (23.33%) - LeetHub

---
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.go
@@ -0,0 +1,36 @@
+/*
+7
+0,1,2,3,4
+0,2,1,1,1
+
+[4,7,6,4,4,2,2,4,8,8]
+2,4,6,7,8
+2,4,1,1,2
+4,16,6,7,16
+  
+*/
+func maxIceCream(costs []int, coins int) int {
+    counter := [100001]int{}
+    for _, n := range costs {
+        counter[n]++
+    }
+
+    var res int
+    for i := 1; i < len(counter); i++ {
+        c := counter[i]
+
+        // if c != 0 && i <= coins {
+        //     res += c
+        //     coins -= i * c
+        // }
+        
+        // InnerLoop:
+            for coins != 0 && c != 0 && i <= coins {
+                res++
+                c--
+                coins -= i
+            }
+    }
+
+    return res
+}
