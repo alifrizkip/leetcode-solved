4efc36f09bc3971b79e82218d2c4089524df79e0
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-15 13:53:58 +0700

    Time: 0 ms (100%), Space: 2.1 MB (5.88%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.go
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.go
@@ -1,15 +1,19 @@
-/*
-((())
-
-*/
 func minAddToMakeValid(s string) int {
     st := []string{}
     for _, v := range s {
         c := string(v)
 
+        // (
+        // push to stack
         if c == "(" {
             st = append(st, c)
-        } else {
+            continue
+        }
+
+        // )
+        // if 'stack.last==(' then 'stack.pop'
+        // else push ) to stack
+        if c == ")" {
             if len(st) > 0 && st[len(st)-1] == "(" {
                 st = st[:len(st)-1]
             } else {
