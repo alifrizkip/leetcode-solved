b76e4db02f0324aed5e711299c6e4d3b77222870
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-19 22:50:22 +0700

    Time: 0 ms (100%), Space: 2.3 MB (5.07%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.go
+++ 0409-longest-palindrome/0409-longest-palindrome.go
@@ -4,17 +4,13 @@ func longestPalindrome(s string) int {
         dicts[string(c)]++
     }
 
-    var res int
-    var one int
+    var res, one int
     for _, val := range dicts {
         if val % 2 == 0 {
             res += val
         } else {
             res += val - 1
-
-            if one == 0 {
-                one = 1
-            }
+            one = 1
         }
     }
     return res+one
