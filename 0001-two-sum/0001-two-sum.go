2c15d5323c179fe4ee6eb6dcf8b21f76287c2997
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-20 16:08:53 +0700

    Time: 4 ms (86.82%), Space: 5.6 MB (9.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,20 @@
+/*
+{
+    3: 0
+}
+*/
+func twoSum(nums []int, target int) []int {
+    hm := map[int]int{}
+    res := []int{}
+
+    for i, num := range nums {
+        val, ok := hm[target-num]
+        if ok {
+            res = []int{val, i}
+        } else {
+            hm[num] = i
+        }
+    }
+
+    return res
+}
