0dea977cadb4824a905ce2fdd53dbda4203ce5f9
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-26 15:23:04 +0700

    Time: 19 ms (81.73%), Space: 6.9 MB (74.64%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.go
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -1,4 +1,5 @@
 func merge(intervals [][]int) [][]int {
+    // sort intervals
     sort.Slice(intervals, func(a, b int) bool {
         return (intervals[a][0] < intervals[b][0]) ||
             ( (intervals[a][0] == intervals[b][0]) &&
@@ -7,18 +8,21 @@ func merge(intervals [][]int) [][]int {
 
     var left, right [][]int = intervals[0:1], intervals[1:]
     
+    // merge right to left    
     for len(left) > 0 && len(right) > 0 {
+        // left[last], right[first]
         leftItem, rightItem := left[len(left)-1], right[0]
 
         if isOverlap(leftItem, rightItem) {
             newItem := mergeOne(leftItem, rightItem)
             
+            // len(left) == 2 && newItem == right[first]
             if (newItem[0] == rightItem[0] &&
                 newItem[1] == rightItem[1]) && len(left) > 1 {
-                left = left[:len(left)-1]
+                left = left[:len(left)-1] // remove left[last]
             } else {
                 left[len(left)-1] = newItem
-                right = right[1:]
+                right = right[1:] // remove right[first]
             }
         } else {
             left = append(left, rightItem)
@@ -30,8 +34,6 @@ func merge(intervals [][]int) [][]int {
 }
 
 func isOverlap(a, b []int) bool {
-    // return a[0] < b[1] && b[0] < a[1]
-    // return a[1] >= b[0]
     if a[1] < b[1] {
         return a[1] >= b[0]
     }
