bf4242fa9dfc6e0e081857ce4b9cf5316452bebf
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-08-26 15:18:13 +0700

    Time: 13 ms (97.86%), Space: 6.6 MB (90.28%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -0,0 +1,54 @@
+func merge(intervals [][]int) [][]int {
+    sort.Slice(intervals, func(a, b int) bool {
+        return (intervals[a][0] < intervals[b][0]) ||
+            ( (intervals[a][0] == intervals[b][0]) &&
+                (intervals[a][1] < intervals[b][1]) )
+    })
+
+    var left, right [][]int = intervals[0:1], intervals[1:]
+    
+    for len(left) > 0 && len(right) > 0 {
+        leftItem, rightItem := left[len(left)-1], right[0]
+
+        if isOverlap(leftItem, rightItem) {
+            newItem := mergeOne(leftItem, rightItem)
+            
+            if (newItem[0] == rightItem[0] &&
+                newItem[1] == rightItem[1]) && len(left) > 1 {
+                left = left[:len(left)-1]
+            } else {
+                left[len(left)-1] = newItem
+                right = right[1:]
+            }
+        } else {
+            left = append(left, rightItem)
+            right = right[1:]
+        }
+    }
+
+    return left
+}
+
+func isOverlap(a, b []int) bool {
+    // return a[0] < b[1] && b[0] < a[1]
+    // return a[1] >= b[0]
+    if a[1] < b[1] {
+        return a[1] >= b[0]
+    }
+
+    return b[1] >= a[0]
+}
+
+func mergeOne(a, b []int) []int {
+    start, end := a[0], b[1]
+
+    if b[0] < start {
+        start = b[0]
+    }
+
+    if a[1] > end {
+        end = a[1]
+    }
+
+    return []int{start, end}
+}
