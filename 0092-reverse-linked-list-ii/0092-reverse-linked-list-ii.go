52fc3674cb2eb23e13257c20d4544536571f0eff
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-09-07 21:12:51 +0700

    Time: 1 ms (77.48%), Space: 2.1 MB (96.7%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.go
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.go
@@ -9,10 +9,11 @@ func reverseBetween(head *ListNode, left int, right int) *ListNode {
     arrVal :=[]int{}
 
     node := head
-    i := 0
+    i := 0 // node counter
     for node != nil {
         i++
 
+        // save val between left right to arr
         if i >= left && i <= right {
             arrVal = append(arrVal, node.Val)
         }
@@ -28,6 +29,7 @@ func reverseBetween(head *ListNode, left int, right int) *ListNode {
     for node != nil {
         i++
 
+        // change node.Val from end of arr
         if i >= left && i <= right {
             node.Val = arrVal[len(arrVal)-1]
             arrVal = arrVal[:len(arrVal)-1]
