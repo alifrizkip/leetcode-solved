a3b7842e2bd93883d057106548ab340a26e283ee
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-09-09 06:55:09 +0700

    Time: 3 ms (67.4%), Space: 2.9 MB (94.14%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    for node != nil {
+        if node.Next != nil {
+            node.Val = node.Next.Val
+        }
+
+        if node.Next.Next == nil {
+            node.Next = nil
+        }
+
+        node = node.Next
+    }
+}
