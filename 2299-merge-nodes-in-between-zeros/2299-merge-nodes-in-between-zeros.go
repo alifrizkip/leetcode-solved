f47c57eba17755965b2dea5b50ea14c879fc40bb
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-09-09 06:12:36 +0700

    Time: 335 ms (77.08%), Space: 15.8 MB (83.33%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.go
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.go
@@ -19,22 +19,23 @@ func mergeNodes(head *ListNode) *ListNode {
 
     var currSum int
     for head != nil {
-        tmpHead := head
-
         currSum += head.Val
-        head = head.Next
 
-        if tmpHead.Val == 0 && currSum > 0 {
+        if head.Val == 0 && currSum > 0 {
             node.Val = currSum
             node.Next = &ListNode{}
             
-            if head == nil {
+            // end of head
+            // shoud be end of node
+            if head.Next == nil {
                 node.Next = nil
             }
 
             node = node.Next
             currSum = 0
         }
+
+        head = head.Next
     }
 
     return res.Next
