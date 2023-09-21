c731f54ac786d097a146a5d9c770a7cf44b7b5f0
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2023-09-21 21:47:31 +0700

    Time: 11 ms (69.95%), Space: 5.6 MB (43.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -1,6 +1,22 @@
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-    arr := append(nums1, nums2...)
-    sort.Ints(arr)
+    arr := make([]int, 0, len(nums1)+len(nums2))
+
+    i, j := 0, 0
+    for i < len(nums1) && j < len(nums2) {
+        if nums1[i] < nums2[j] {
+            arr = append(arr, nums1[i])
+            i++
+        } else {
+            arr = append(arr, nums2[j])
+            j++
+        }
+    }
+    if i < len(nums1) {
+        arr = append(arr, nums1[i:]...)
+    }
+    if j < len(nums2) {
+        arr = append(arr, nums2[j:]...)
+    }
 
     arrL := len(arr)
     if arrL % 2 == 1 {
