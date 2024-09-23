16f6062a3f61d6903002a2bd04c1fb95aed5899a
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2024-09-24 06:14:47 +0700

    Time: 6 ms (97.12%), Space: 6.9 MB (66.91%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.go
@@ -0,0 +1,19 @@
+func firstPalindrome(words []string) string {
+    for _, word := range words {
+        if isPalindromic(word) {
+            return word
+        }
+    }
+    
+    return ""
+}
+
+func isPalindromic(word string) bool {
+    for i := 0; i < len(word) / 2; i++ {
+        if word[i] != word[len(word)-1-i] {
+            return false
+        }
+    }
+    
+    return true
+}
