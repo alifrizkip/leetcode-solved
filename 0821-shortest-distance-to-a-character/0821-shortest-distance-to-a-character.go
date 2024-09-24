51ed8f34a5235b9ad77cb1112d8d9ca64e7900f5
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2024-09-24 09:02:17 +0700

    Time: 2 ms (68.75%), Space: 3 MB (11.25%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.go
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.go
@@ -8,39 +8,27 @@ func shortestToChar(s string, c byte) []int {
         }
     }
     
-    fmt.Println(cPos)
-    
     var cI = 0
     for i := 0; i < len(s); i++ {
         far := calcFar(cPos[cI], i)
         nextFar := far
-        // nextFar := calcFar(cPos[cI+1], i)
         
         if cI < len(cPos)-1 {
             nextFar = calcFar(cPos[cI+1], i)
         }
         
-        if abs(far) < abs(nextFar) {
-            res = append(res, abs(far))
+        if far < nextFar {
+            res = append(res, far)
         } else {
             if cI < len(cPos) - 1 {
                 cI++
             }
             
-            res = append(res, abs(nextFar))
+            res = append(res, nextFar)
         }
     }
-    
-    
-    return res
-}
 
-func abs(n int) int {
-    if n < 0 {
-        return n * -1
-    }
-    
-    return n
+    return res
 }
 
 func calcFar(a, b int) int {
